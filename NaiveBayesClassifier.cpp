9f5bc0d9be4338df655ecc0e06cb573f7cbe98ad
Steven Smith <ssmith563@umail.ucsb.edu>
2022-02-17 02:32:10 -0800

    finished training

--- NaiveBayesClassifier.cpp
+++ NaiveBayesClassifier.cpp
@@ -4,6 +4,7 @@
 #include "NaiveBayesClassifier.h"
 #include <fstream>
 #include <sstream>
+#include <cmath>
 
 using namespace std;
 
@@ -11,60 +12,203 @@ NaiveBayesClassifier::NaiveBayesClassifier(){
     
 }
 
+void NaiveBayesClassifier::updateFeatureCDF(double age, string gender, double height, 
+    double weight, double bodyfat, double diastolic, double systolic, 
+    double grip_force, double sit_and_bend_forward, double sit_up_count, 
+    double broad_jump, int grade){
+
+    
+
+    if(grade == 1){
+        updateFeatureUtil(&age_y, age);
+        updateGenderUtil(&gender_y, gender);
+        updateFeatureUtil(&height_y, height);
+        updateFeatureUtil(&weight_y, weight);
+        updateFeatureUtil(&bodyfat_y, bodyfat);
+        updateFeatureUtil(&diastolic_y, diastolic);
+        updateFeatureUtil(&systolic_y, systolic);
+        updateFeatureUtil(&grip_force_y, grip_force);
+        updateFeatureUtil(&sit_and_bend_forward_y, sit_and_bend_forward);
+        updateFeatureUtil(&sit_up_count_y, sit_up_count);
+        updateFeatureUtil(&broad_jump_y, broad_jump);
+    }
+    else{
+        updateFeatureUtil(&age_n, age);
+        updateGenderUtil(&gender_n, gender);
+        updateFeatureUtil(&height_n, height);
+        updateFeatureUtil(&weight_n, weight);
+        updateFeatureUtil(&bodyfat_n, bodyfat);
+        updateFeatureUtil(&diastolic_n, diastolic);
+        updateFeatureUtil(&systolic_n, systolic);
+        updateFeatureUtil(&grip_force_n, grip_force);
+        updateFeatureUtil(&sit_and_bend_forward_n, sit_and_bend_forward);
+        updateFeatureUtil(&sit_up_count_n, sit_up_count);
+        updateFeatureUtil(&broad_jump_n, broad_jump);
+    }
+
+}
+
+void NaiveBayesClassifier::setFeatureCDF(double age, std::string gender, double height, 
+    double weight, double bodyfat, double diastolic, double systolic, 
+    double grip_force, double sit_and_bend_forward, double sit_up_count, 
+    double broad_jump, int grade){
+
+    if(grade == 1){
+        age_y.mean = age;
+
+        if(gender == "M"){
+            gender_y.males = 1;
+        }
+
+        height_y.mean = height;
+        
+        weight_y.mean = weight;
+
+        bodyfat_y.mean = bodyfat;
+
+        diastolic_y.mean = diastolic;
+        
+        systolic_y.mean = systolic;
+        
+        grip_force_y.mean = grip_force;
+        
+        sit_and_bend_forward_y.mean = sit_and_bend_forward;
+        
+        sit_up_count_y.mean = sit_up_count;
+        
+        broad_jump_y.mean = broad_jump;
+    }
+    else{
+        age_n.mean = age;
+        
+        if(gender == "M"){
+            gender_n.males = 1;
+        }
+
+        height_n.mean = height;
+        
+        weight_n.mean = weight;
+        
+        bodyfat_n.mean = bodyfat;
+        
+        diastolic_n.mean = diastolic;
+        
+        systolic_n.mean = systolic;
+        
+        grip_force_n.mean = grip_force;
+        
+        sit_and_bend_forward_n.mean = sit_and_bend_forward;
+        
+        sit_up_count_n.mean = sit_up_count;
+        
+        broad_jump_n.mean = broad_jump;
+    }
+
+}
+
+void NaiveBayesClassifier::updateFeatureUtil(features *feature, double x){
+    
+    feature->n = feature->n+1;
+    
+    feature->oldMean = feature->mean;
+
+    feature->mean = ((feature->mean * (feature->n-1))+x)/feature->n;
+
+    feature->stdVar = sqrt( ( (feature->n-2)*feature->stdVar*feature->stdVar + (x - feature->mean)*(x - feature->oldMean) )/(feature->n-1));
+}
+
+void NaiveBayesClassifier::updateGenderUtil(genderFeature *gender, string genderValue){
+    gender->n = gender->n+1;
+    if(genderValue == "M"){
+        gender->males = gender->males+1;
+    }
+}
+
+void NaiveBayesClassifier::printFeatureAttributes(features feature){
+    cout<<"Mean: "<<feature.mean<<"\n";
+    cout<<"N: "<<feature.n<<"\n";
+    cout<<"Standard Deviation: "<<feature.stdVar<<"\n";
+}
+
+features NaiveBayesClassifier::getFeature(){
+    return height_n;
+}
+
 int main(int argc,char* argv[])//int argc,char* argv[]
 {
+
+    NaiveBayesClassifier NBC;
+
     ifstream inFile1;
     inFile1.open( argv[1] );
 
     string line;
     
-    string age;
+    string num1;
     string gender;
-    string height; 
-    string weight; 
-    string bodyfat; 
-    string diastolic;
-    string systolic;
-    string grip_force; 
-    string sit_and_bend_forward; 
-    string sit_up_count;
-    string broad_jump;
-    string grade;
+    string num2; 
+    string num3; 
+    string num4; 
+    string num5;
+    string num6;
+    string num7; 
+    string num8; 
+    string num9;
+    string num10;
+    string num11;
 
-    
+    int grade_0_flag = 1;
+    int grade_1_flag = 1;
+
     while ( getline(inFile1,line) )
     {
         istringstream linestream(line);
-        //while(linestream.good()){
-        getline(linestream, age, ',');
-        getline(linestream, gender, ',');
-        getline(linestream, height, ',');
-        getline(linestream, weight, ',');
-        getline(linestream, bodyfat, ',');
-        getline(linestream, diastolic, ',');
-        getline(linestream, systolic, ',');
-        getline(linestream, grip_force, ',');
-        getline(linestream, sit_and_bend_forward, ',');
-        getline(linestream, sit_up_count, ',');
-        getline(linestream, broad_jump, ',');
-        getline(linestream, grade, ',');
 
-        double number1 = stod(age);
-        double number2 = stod(height);
-        double number3 = stod(weight);
-        double number4 = stod(bodyfat);
-        double number5 = stod(diastolic);
-        double number6 = stod(systolic);
-        double number7= stod(grip_force);
-        double number8 = stod(sit_and_bend_forward);
-        double number9 = stod(sit_up_count);
-        double number10 = stod(broad_jump);
-        int number11 = stoi(grade);
+        getline(linestream, num1, ',');
+        getline(linestream, gender, ',');
+        getline(linestream, num2, ',');
+        getline(linestream, num3, ',');
+        getline(linestream, num4, ',');
+        getline(linestream, num5, ',');
+        getline(linestream, num6, ',');
+        getline(linestream, num7, ',');
+        getline(linestream, num8, ',');
+        getline(linestream, num9, ',');
+        getline(linestream, num10, ',');
+        getline(linestream, num11, ',');
+
+        double age = stod(num1);
+        double height = stod(num2);
+        double weight = stod(num3);
+        double bodyfat = stod(num4);
+        double diastolic = stod(num5);
+        double systolic = stod(num6);
+        double grip_force= stod(num7);
+        double sit_and_bend_forward = stod(num8);
+        double sit_up_count = stod(num9);
+        double broad_jump = stod(num10);
+        int grade = stoi(num11);
+
         
-        cout<<number2<<" ";
+
+        if(grade == 0 && grade_0_flag == 1){
+            grade_0_flag = 0;
+            NBC.setFeatureCDF(age, gender, height, weight, bodyfat, diastolic, systolic, grip_force, sit_and_bend_forward, sit_up_count, broad_jump, grade);
+
+        }
+        else if(grade == 1 && grade_1_flag == 1){
+            grade_1_flag = 0;
+            NBC.setFeatureCDF(age, gender, height, weight, bodyfat, diastolic, systolic, grip_force, sit_and_bend_forward, sit_up_count, broad_jump, grade);
+        }
+        else{
+            NBC.updateFeatureCDF(age, gender, height, weight, bodyfat, diastolic, systolic, grip_force, sit_and_bend_forward, sit_up_count, broad_jump, grade);
+        }
         
     }
     inFile1.close();
+
+    features test = NBC.getFeature();
+    NBC.printFeatureAttributes(test);
 
     
     
--- NaiveBayesClassifier.h
+++ NaiveBayesClassifier.h
@@ -5,14 +5,77 @@
 #include <vector>
 
 
+struct features{
+    double mean=0;
+    double oldMean=0;
+    double n=1;
+    double stdVar=0;
+};
+
+struct genderFeature{
+    double n=1;
+    double males=1;
+};
+
 class NaiveBayesClassifier
 {
     
     private:
 
+    features age_y;
+    features age_n;
+
+    genderFeature gender_y;
+    genderFeature gender_n;
+
+    features height_y;
+    features height_n;
+
+    features weight_y;
+    features weight_n;
+
+    features bodyfat_y;
+    features bodyfat_n;
+
+    features diastolic_y;
+    features diastolic_n;
+
+    features systolic_y;
+    features systolic_n;
+
+    features grip_force_y;
+    features grip_force_n;
+
+    features sit_and_bend_forward_y;
+    features sit_and_bend_forward_n;
+
+    features sit_up_count_y;
+    features sit_up_count_n;
+
+    features broad_jump_y;
+    features broad_jump_n;
+
     public:
 
     NaiveBayesClassifier();
+
+    void updateFeatureCDF(double age, std::string gender, double height, 
+    double weight, double bodyfat, double diastolic, double systolic, 
+    double grip_force, double sit_and_bend_forward, double sit_up_count, 
+    double broad_jump, int grade);
+
+    void setFeatureCDF(double age, std::string gender, double height, 
+    double weight, double bodyfat, double diastolic, double systolic, 
+    double grip_force, double sit_and_bend_forward, double sit_up_count, 
+    double broad_jump, int grade);
+
+    void updateFeatureUtil(features *feature, double x);
+
+    void updateGenderUtil(genderFeature *gender, std::string genderValue);
+
+    void printFeatureAttributes(features feature);
+
+    features getFeature();
 
     
 };
